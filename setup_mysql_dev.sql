75c623303c1ae340c3e608f7a02bb4e7a37556b1
nathen321 <walidjalaly43@gmail.com>
2024-04-17 03:55:12 -0700

    corretion of sql code

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,6 @@
 -- Create states table in hbtn_0e_0_usa with some data
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-USE hbnb_dev_db;
-CREATE USER 'hbnb_dev'@'localhost' IF NOT IN EXISTS IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost'
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost'
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'
+
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
