9277a8a40d1021a7cc4eecdba5041ae48db3d698
nathen321 <walidjalaly43@gmail.com>
2024-04-17 05:29:24 -0700

    test env created

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- Create states table in hbtn_0e_0_usa with some data
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
