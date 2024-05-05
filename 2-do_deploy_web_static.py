501ba377f98a5bb5e91e3859f4ad0b25a486436c
nathen321 <walidjalaly43@gmail.com>
2024-05-05 13:28:26 -0700

    taske 2 test 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,11 +5,15 @@ folder of your AirBnB Clone repo, using the function do_pack.
 """
 
 from datetime import datetime
+from fabric.api import run
+from fabric.api import sudo
 from fabric.api import *
 from os.path import isdir
 from os.path import isfile
 
+
 env.hosts = ['100.26.239.249', '34.232.77.141']
+
 
 def do_pack():
     """script that generates a .tgz archive"""
@@ -23,6 +27,7 @@ def do_pack():
     except Expecte:
         return None
 
+
 def do_deploy(archive_path):
     """script to deploy web serv"""
     try:
@@ -30,10 +35,17 @@ def do_deploy(archive_path):
             file = archive_path.split("/")[-1]
             name = file.split(".")[0]
             put(archive_path, "/tmp/{}".format(file))
-            sudo("tar zxvf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
+            sudo("rm -rf /data/web_static/releases/{}/".format(name))
+            sudo("mkdir -p /data/web_static/releases/{}/".format(name))
+            sudo(
+                    "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+                        file, name))
             sudo("rm -r /tmp/{}".format(file))
             sudo("rm -rf /data/web_static/current")
-            sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
+            s = "ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            sudo(s.format(name))
             return True
+        else:
+            return False
     except Expecte:
         return False
