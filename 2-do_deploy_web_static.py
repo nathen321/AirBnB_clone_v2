f381b306f13bc8612efe8baf8755a9d36581c5fd
nathen321 <walidjalaly43@gmail.com>
2024-05-05 13:50:59 -0700

    taske 2 test 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -30,22 +30,20 @@ def do_pack():
 
 def do_deploy(archive_path):
     """script to deploy web serv"""
+     if exists(archive_path) is False:
+        return False
     try:
-        if isfile(archive_path):
-            file = archive_path.split("/")[-1]
-            name = file.split(".")[0]
-            put(archive_path, "/tmp/{}".format(file))
-            sudo("rm -rf /data/web_static/releases/{}/".format(name))
-            sudo("mkdir -p /data/web_static/releases/{}/".format(name))
-            sudo(
-                    "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-                        file, name))
-            sudo("rm -r /tmp/{}".format(file))
-            sudo("rm -rf /data/web_static/current")
-            s = "ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            sudo(s.format(name))
-            return True
-        else:
-            return False
-    except Expecte:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
         return False
