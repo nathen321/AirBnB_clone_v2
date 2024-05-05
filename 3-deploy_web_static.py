c786757f754dadd61061f83a9eea9ef5c322dfcf
nathen321 <walidjalaly43@gmail.com>
2024-05-05 14:16:05 -0700

    test 8

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -12,6 +12,19 @@ from os.path import exists
 env.hosts = ['100.26.239.249', '34.232.77.141']
 
 
+def do_pack():
+    """script that generates a .tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except Exception as e:
+        return None
+
+
 def do_deploy(archive_path):
     """script to deploy web serv"""
     if exists(archive_path) is False:
