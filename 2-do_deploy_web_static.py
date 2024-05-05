bb618d012b67d9acc695fcaa1d632fcd4813569a
nathen321 <walidjalaly43@gmail.com>
2024-05-05 10:50:32 -0700

    task 3 test

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the contents of the web_static
+folder of your AirBnB Clone repo, using the function do_pack.
+"""
+
+from datetime import datetime
+from fabric.api import *
+from os.path import isdir
+from os.path import isfile
+
+env.hosts = ['100.26.239.249', '34.232.77.141']
+
+def do_pack():
+    """script that generates a .tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except Expecte:
+        return None
+
+def do_deploy(archive_path):
+    """script to deploy web serv"""
+    try:
+        if isfile(archive_path):
+            file = archive_path.split("/")[-1]
+            name = file.split(".")[0]
+            put(archive_path, "/tmp/{}".format(file))
+            sudo("tar zxvf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
+            sudo("rm -r /tmp/{}".format(file))
+            sudo("rm -rf /data/web_static/current")
+            sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
+            return True
+    except Expecte:
+        return False
