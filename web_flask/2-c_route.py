8377ad08e35f99606f1b0c7d7de8c0755af05dd6
nathen321 <walidjalaly43@gmail.com>
2024-05-19 11:00:55 -0700

    test 2 task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def Cisfun(text):
-    return "C {}".format(escape(text))
+    return "C {}".format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
