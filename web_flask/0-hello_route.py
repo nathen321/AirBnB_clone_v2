f0ef3980913b8565ebc0d2d170f27f8031ccb273
nathen321 <walidjalaly43@gmail.com>
2024-05-19 09:27:34 -0700

    first step

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A simple  minimal Flask application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+
+app.run(host='0.0.0.0', port='5000')
