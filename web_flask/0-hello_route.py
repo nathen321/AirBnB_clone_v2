46686492b07e35a2500569ef1f35fe40fd727364
nathen321 <walidjalaly43@gmail.com>
2024-05-19 09:35:04 -0700

    first step test 2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""A simple  minimal Flask application"""
+"""
+A simple  minimal Flask application
+"""
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_world():
+def hello_hbnb():
     return "Hello HBNB!"
 
 
-app.run(host='0.0.0.0', port='5000')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
