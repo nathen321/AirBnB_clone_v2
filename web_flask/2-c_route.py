64eb5fcf891280fc25e13c19222b8024a312533e
nathen321 <walidjalaly43@gmail.com>
2024-05-19 10:57:46 -0700

    test task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A simple  minimal Flask application
+"""
+
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def Cisfun(text):
+    return "C {}".format(escape(text))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
